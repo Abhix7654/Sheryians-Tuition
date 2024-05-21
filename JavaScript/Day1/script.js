68b5e1d513b1a1efcb4d19c8c7e0addd378b868c
abhishek <rajabhishek7654@gmail.com>
2024-05-21 09:11:41 +0530

    javascript

---
+++ JavaScript/Day1/script.js
@@ -0,0 +1,24 @@
+// variables and datatype
+// scope
+
+// process of crating datatype
+
+// 1.var/let/const -keyword
+// 2.variable/container name
+// 3. =
+// 4.value/info
+
+// eg:-
+// var a=12;
+
+
+// naming convension of variables
+
+// 1.must be related to the  Data
+// 2.variable name must start with small/capital letter of "_"
+// 3.must not start with numerical value thugh can be used after first letter
+// 4.variable name must be single word
+// 5.must use case pattern if variable name is more than a word
+// snake_case_pattern
+// camelCasePattern
+// PascalCasePattern
