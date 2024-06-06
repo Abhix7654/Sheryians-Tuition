64a73df4b964793bc667c11aa26bb5e2f9c92116
abhishek <rajabhishek7654@gmail.com>
2024-06-06 09:24:23 +0530

    js qs

---
+++ JavaScript/Day2/script.js
@@ -0,0 +1,28 @@
+
+// let n=+prompt("enter any no");
+// console.log(typeof n);
+
+// let v=prompt("enter user name" );
+// console.log(v)
+// console.log(`user name is ${v}`);
+
+// var a=prompt();
+// a=Number(a);
+
+// var b=prompt();
+// b=Number(b);
+
+// console.log(`The sum of ${a} & ${b} is ${a+b}`)
+
+
+// var a=prompt("enter 1st one marks");
+// a=Number(a);
+// var b=prompt("enter 2nd one marks");
+// b=Number(b);
+// var c=prompt("enter 3rd one marks");
+// c=Number(c);
+// var d=a+b+c;
+// var e=((d/300)*100).toFixed(2)
+
+// console.log(`Hello Abhishek your total score is ${a+b+c} with ${e}`);
+
