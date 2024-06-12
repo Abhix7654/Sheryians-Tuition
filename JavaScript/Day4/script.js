aa4c6a41d36440437c8c72787b9efb86c738d5d8
abhishek <rajabhishek7654@gmail.com>
2024-06-12 09:29:44 +0530

    js

---
+++ JavaScript/Day4/script.js
@@ -0,0 +1,22 @@
+// Escape sequence
+// \n change line
+// \t add tab
+// \b add backspace
+// \r 
+
+//n double slash is used to escape special character and print only one slash
+///t will print only two slash
+
+// console.log(`https:\\localhost:3000\\new\\tab`);
+// console.log("https:\\localhost:3000\\new\\tab");
+
+// console.log("hello\nworld")
+
+// //methods
+
+// var s="Hello  World";
+
+// console.log(s.length);
+
+
+
