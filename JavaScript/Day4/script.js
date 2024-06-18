0363d2b6ebfd157ed8caf78145a0d36be2911ffd
abhishek <rajabhishek7654@gmail.com>
2024-06-18 17:03:11 +0530

    js

--- JavaScript/Day4/script.js
+++ JavaScript/Day4/script.js
@@ -22,12 +22,12 @@
 
 // print vs return 
 
-function sum(a,b){
-sum=a+b;
-return sum;
+// function sum(a,b){
+// sum=a+b;
+// return sum;
 
-}
-console.log(sum(2,3));
+// }
+// console.log(sum(2,3));
 
 
 // variable length parameter/arguments
@@ -35,13 +35,48 @@ console.log(sum(2,3));
 // it should be the last parameter int function
 // there must not be more than one rest operator
 
-function hobbies(name,age,...h){
-        console.log(name,age);
-        return h;
+// function hobbies(name,age,...h){
+//         console.log(name,age);
+//         return h;
 
-}
+// }
 
-let result=hobbies("joohn",34,99,"ndosmd");
+// let result=hobbies("joohn",34,99,"ndosmd");
 
-console.log(result);
+// console.log(result);
 
+let n=prompt("Enter no");
+n=Number(n);
+
+function fibonacciSeries(n) {
+       
+        let fib = [0, 1];
+    
+       
+        for (let i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+    
+        return fib;
+    }
+
+   
+
+    console.log(fibonacciSeries(n));
+
+
+// function primeno(n) {
+//         let c = 0;
+//         for (let i = 1; i <= n; i++) {
+//             if (n % i == 0) {
+//                 c++;
+//             }
+//         }
+//         if (c == 2) {
+//             console.log("It's prime");
+//         } else {
+//             console.log("It's not prime");
+//         }
+//     }
+    
+//     primeno(n);
