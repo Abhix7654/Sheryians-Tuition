9a0c7c51f991b5de8b6dcd4b5bb36c87dc4bf609
abhishek <rajabhishek7654@gmail.com>
2024-06-19 09:21:55 +0530

    fn

--- JavaScript/Day5/script.js
+++ JavaScript/Day5/script.js
@@ -1,10 +1,21 @@
-// Hoisting
+// let c= (s,n) =>{
+//         console.log(s+n);
+// }
+// c(3,9);  Arrow function
 
-console.log(a);//cannot acces before initialization
-const a=4;
+// // Hoisting
 
-console.log(b);//cannot acces before initialization
-let b=4;
+// console.log(a);//cannot acces before initialization(TDZ)
+// const a=4;
 
-console.log(c);//undefined
-var c=4;
+// console.log(b);//cannot acces before initialization(Temperol Dead Zone)
+// let b=4;
+
+// console.log(c);//undefined
+// var c=4;
+
+sum(3,7);
+
+function sum(a,b){
+        console.log(a+b);
+}
