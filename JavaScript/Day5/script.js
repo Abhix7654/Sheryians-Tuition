c94a3ce78a78ac8f106420f2ac9494797a4abcc0
abhishek <rajabhishek7654@gmail.com>
2024-06-19 09:12:30 +0530

    js

--- JavaScript/Day5/script.js
+++ JavaScript/Day5/script.js
@@ -1,9 +1,9 @@
 // Hoisting
 
-console.log(a);//cannot acces before initalization
+console.log(a);//cannot acces before initialization
 const a=4;
 
-console.log(b);//cannot acces before initalization
+console.log(b);//cannot acces before initialization
 let b=4;
 
 console.log(c);//undefined
