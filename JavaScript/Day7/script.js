2694c4f5b1ea01fd567cc334846bb900c47f9ff5
abhishek <rajabhishek7654@gmail.com>
2024-06-27 09:15:41 +0530

    AJAX

--- JavaScript/Day7/script.js
+++ JavaScript/Day7/script.js
@@ -17,3 +17,13 @@ let x=setInterval(()=>{
 },100);
 
 console.log(x);
+
+
+// api is given by backend and to implement api in frontend we use the concept of AJAX which is theoretical and to
+// implement it we use "fetch api"/axios in frontend
+let userapi=`https://jsonplaceholder.typicode.com/users`;
+
+fetch(userapi)
+.then((e)=>e.json())
+.then((r)=>console.log(r))
+.catch((err)=>console.log(error));
