f6a1caf4bc267fb0cd3897e1a0de523c9c9b5b07
abhishek <rajabhishek7654@gmail.com>
2024-07-03 23:33:09 +0530

    slidebar

---
+++ practise session/2nd/script.js
@@ -0,0 +1,7 @@
+document.getElementById("menuButton").onclick = function() {
+        document.getElementById("sidebar").style.width = "50%";
+    }
+
+    document.getElementById("closeButton").onclick = function() {
+        document.getElementById("sidebar").style.width = "0";
+    }
