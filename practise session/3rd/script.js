3e76b8b019fa5ed76dbc1164e244b194a926e7cc
abhishek <rajabhishek7654@gmail.com>
2024-07-05 11:08:24 +0530

    gallery

--- practise session/3rd/script.js
+++ practise session/3rd/script.js
@@ -1,12 +1,23 @@
-const disp=document.querySelector("#view");
+const disp = document.querySelector("#view");
+let i = 0; 
 document.querySelectorAll(".img").forEach(img => {
-        img.addEventListener("click", () => {
-                 const style = window.getComputedStyle(img);
-                const bgImage = style.backgroundImage;
-                disp.style.display="initial";
-                document.querySelector("#view").style.backgroundImage = bgImage;
-       
-        });
+    img.addEventListener("click", (event) => {
+        if (i === 0) {
+            const style = window.getComputedStyle(img);
+            const bgImage = style.backgroundImage;
+            disp.style.display = "initial";
+            i = 1;
+            document.querySelector("#main").style.filter = "blur(8px)";
+            disp.style.backgroundImage = bgImage;
+            event.stopPropagation();  
+        }
     });
-   
-   
+});
+
+document.querySelector("#main").addEventListener("click", (event) => {
+    if (i === 1 && !disp.contains(event.target)) {
+        disp.style.display = "none";
+        document.querySelector("#main").style.filter = "none";
+        i = 0;
+    }
+});
