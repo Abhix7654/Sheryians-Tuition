40e9437677381e58a245394ee0436049b088fb8d
abhishek <rajabhishek7654@gmail.com>
2024-07-17 09:28:39 +0530

    shery js

--- practise session/mouse follower/script.js
+++ practise session/mouse follower/script.js
@@ -1,10 +1,12 @@
-var main=document.querySelector('.main');
-var cursor=document.querySelector(".cursor");
+// var main=document.querySelector('.main');
+// var cursor=document.querySelector(".cursor");
 
-main.addEventListener('mousemove',function(e){
-        var x=e.clientX;
-        var y=e.clientY;
-        cursor.style.left=x+'px';
-        cursor.style.top=y+'px';
+// main.addEventListener('mousemove',function(e){
+//         var x=e.clientX;
+//         var y=e.clientY;
+//         cursor.style.left=x+'px';
+//         cursor.style.top=y+'px';
        
-});
+// });
+
+Shery.mouseFollower();
