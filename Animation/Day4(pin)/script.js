ea45e974fbca61c050a52ba09507f6509e657e0c
abhishek <rajabhishek7654@gmail.com>
2024-07-25 16:09:29 +0530

    scroll trigger pin

---
+++ Animation/Day4(pin)/script.js
@@ -0,0 +1,26 @@
+gsap.to("#page2 h1",{
+        transform:"translateX(-150%)",
+        
+ scrollTrigger:{
+            trigger:"#page2",
+            scroller:"body",
+            start:"top 0%",
+            end:"top -100%",
+            scrub:1,
+           
+            pin:true,
+        }
+})
+gsap.to("#page3 img",{
+        width:"70%",
+    
+        scrollTrigger:{
+            trigger:"#page3",
+            scroller:"body",
+            start:"top 0%",
+            end:"top -100%",
+            scrub:1,
+           
+            pin:true,
+        }
+})
